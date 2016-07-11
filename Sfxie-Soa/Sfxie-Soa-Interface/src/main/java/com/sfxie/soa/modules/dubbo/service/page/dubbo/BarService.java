b7b87b29b675930b0fd263a164f7d8290c272b61
xsfcjy <940964471@qq.com>
2016-07-11 16:29:04 +0800

    20160711

---
+++ Sfxie-Soa/Sfxie-Soa-Interface/src/main/java/com/sfxie/soa/modules/dubbo/service/page/dubbo/BarService.java
@@ -0,0 +1,8 @@
+package com.sfxie.soa.modules.dubbo.service.page.dubbo;
+
+import com.sfxie.core.service.IBaseService;
+
+public interface BarService extends IBaseService {
+
+	
+}
---
+++ Sfxie-Soa/Sfxie-Soa-Interface/src/main/java/com/sfxie/soa/modules/dubbo/service/page/pojo/SfxiePageBar.java
@@ -0,0 +1,26 @@
+package com.sfxie.soa.modules.dubbo.service.page.pojo;
+
+public class SfxiePageBar {
+	
+	private String name;
+	
+	private String text;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	
+}
